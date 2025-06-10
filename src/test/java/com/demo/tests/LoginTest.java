7462e0ca1e5d3710dd853c812a53ef85bfeb6fb6
Sravan Kumar <sravankota8799@gmail.com>
2025-06-10 14:52:40 +0530

    merging jenkins code

--- src/test/java/com/demo/tests/LoginTest.java
+++ src/test/java/com/demo/tests/LoginTest.java
@@ -1,13 +1,18 @@
 package com.demo.tests;
 
 import com.demo.commonClasses.FuncitonLibrary;
+import com.demo.listeners.MyListener;
 import com.demo.pages.LoginPage;
 import com.demo.pages.SecureAreaPage;
+
+import io.qameta.allure.Allure;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+@Listeners(MyListener.class)
 public class LoginTest extends FuncitonLibrary {
 
 //    private WebDriver driver;
@@ -24,6 +29,7 @@ public class LoginTest extends FuncitonLibrary {
 		  driver.get("https://the-internet.herokuapp.com/login");
 		loginPage = new LoginPage(driver);
 		secureAreaPage = new SecureAreaPage(driver);
+		Allure.addAttachment("My LogMessage", "Browser opened successfully ");
 	}
 
 	@Test
@@ -33,6 +39,7 @@ public class LoginTest extends FuncitonLibrary {
 		loginPage.clickLogin();
 		Assert.assertEquals(secureAreaPage.getHeaderText(), "Secure Area");
 		Assert.assertTrue(loginPage.getFlashMessage().contains("You logged into a secure area!"));
+		Allure.addAttachment("My LogMessage", "Application logged in successfully ");
 	}
 
 	@AfterClass
